c82f0a905c655f7e842145fc0c6dbccf23a81143
Prathmesh Gade <your_github_email@gmail.com>
2026-02-15 15:51:46 +0530

    fixed websocket routing

--- battle/routing.py
+++ battle/routing.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
 from .consumers import BattleConsumer
 
+# WebSocket URL patterns
 websocket_urlpatterns = [
-    re_path(r'ws/battle/(?P<room>\w+)/$', BattleConsumer.as_asgi()),
+    re_path(
+        r'^ws/battle/(?P<room>\w+)/$',
+        BattleConsumer.as_asgi()
+    ),
 ]
